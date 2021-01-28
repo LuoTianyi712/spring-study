c9067173e6de8a157dd18d912004fedb078bf076
LuoTianyi712 <1228478412@qq.com>
2021-01-29 07:56:44 +0800

    模块1 IOC理解

---
+++ src/main/java/com/neusoft/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.neusoft.service;
+
+import com.neusoft.dao.UserDao;
+import com.neusoft.dao.UserDaoImpl;
+
+public class UserServiceImpl implements UserService{
+
+    private UserDao userDao;
+
+    // 利用set实现动态值注入
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void getUser() {
+        userDao.getUser();
+    }
+}
