403ca844b6c466d0e5696b0ef4c070a46f30fc17
LuoTianyi712 <1228478412@qq.com>
2021-01-29 14:02:31 +0800

    模块3 IOC创建对象的方式

---
+++ spring-03-createioc/src/main/java/com/neusoft/pojo/User.java
@@ -0,0 +1,16 @@
+package com.neusoft.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+
+    public User(){
+        System.out.println("User的无参构造");
+    }
+
+    public void show(){
+        System.out.println("name="+name);
+    }
+}
---
+++ spring-03-createioc/src/test/java/ApplicationTest.java
@@ -0,0 +1,14 @@
+import com.neusoft.pojo.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationTest {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        User user = (User) context.getBean("user");
+        user.show();
+
+    }
+}
